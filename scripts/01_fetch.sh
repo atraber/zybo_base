55ba626f5f0829d57b54683c47b848ea0c66bb33
atraber <andytraber@gmail.com>
2014-10-18 16:52:34 +0200

    changed scripts

--- scripts/01_fetch.sh
+++ scripts/01_fetch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Linux
-git clone -b master-next git@github.com:DigilentInc/Linux-Digilent-Dev.git
+git clone -b master-next https://github.com/DigilentInc/Linux-Digilent-Dev.git
 
 # U-Boot
-git clone -b master-next git@github.com:DigilentInc/u-boot-Digilent-Dev.git
+git clone -b master-next https://github.com/DigilentInc/u-boot-Digilent-Dev.git
 
 # device-tree generator for Vivado > 2014.1
-git clone git@github.com:Xilinx/device-tree-xlnx.git
+git clone https://github.com/Xilinx/device-tree-xlnx.git
---
+++ scripts/02_compile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+################################################################################
+# compile u-boot
+################################################################################
+
+cd ./u-boot-Digilent-Dev
+make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi- xilinx_ZYBO_defconfig
+make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+
+# add mkimage to path
+export PATH=$PATH:$(pwd)/tools
+
+cd ..
+
+
+################################################################################
+# Compile Linux
+################################################################################
+cd ./Linux-Digilent-Dev
+
+make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi- xilinx_zynq_defconfig
+make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi-
+
+make ARCH=arm CROSS_COMPILE=arm-xilinx-linux-gnueabi- UIMAGE_LOADADDR=0x8000 uImage
+
+
+cd ..
